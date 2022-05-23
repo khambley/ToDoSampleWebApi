8f39e6607c089481d0aff21c85574500695b80d9
khambley <superdesigngirl@outlook.com>
2022-05-23 13:56:33 -0500

    Refactored to use MVVM design pattern

--- MobileApps/ToDoSampleMobileApp/ToDoSampleMobileApp/ViewModels/MainViewModel.cs
+++ MobileApps/ToDoSampleMobileApp/ToDoSampleMobileApp/ViewModels/MainViewModel.cs
@@ -1,15 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using ToDoSampleAppModels;
 
 namespace ToDoSampleMobileApp.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
-        public List<TodoItem> TodoItems { get; set; }
+        private List<TodoItem> _todoItems;
+
+        public List<TodoItem> TodoItems
+        {
+            get => _todoItems;
+            set
+            {
+                _todoItems = value;
+                OnPropertyChanged();
+            }
+        }
 
         public MainViewModel()
         {
+            GetToDoItems();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void GetToDoItems()
+        {
+            TodoItems = new List<TodoItem>
+            {
+                new TodoItem{
+                    Id = 1,
+                    Title = "Call or go to YMCA and find out about May bill",
+                    IsCompleted = false,
+                    DueDate = DateTime.Now,
+                    UpdatedAt = DateTime.Now
+                },
+                new TodoItem{
+                    Id = 2,
+                    Title = "Get a new pill box",
+                    IsCompleted = false,
+                    DueDate = DateTime.Parse("2022-05-22"),
+                    UpdatedAt = DateTime.Now
+                }
+            };
         }
     }
 }
--- MobileApps/ToDoSampleMobileApp/ToDoSampleMobileApp/Views/MainPage.xaml.cs
+++ MobileApps/ToDoSampleMobileApp/ToDoSampleMobileApp/Views/MainPage.xaml.cs
@@ -5,39 +5,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ToDoSampleAppModels;
+using ToDoSampleMobileApp.ViewModels;
 using Xamarin.Forms;
 
 namespace ToDoSampleMobileApp
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage()
+        public MainPage() //MainViewModel viewModel
         {
             InitializeComponent();
+            //BindingContext = viewModel;
+            
         }
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
+        //protected override void OnAppearing()
+        //{
+        //    base.OnAppearing();
 
-            var todoList = new List<TodoItem>
-            {
-                new TodoItem{
-                    Id = 1,
-                    Title = "Call or go to YMCA and find out about May bill",
-                    IsCompleted = false,
-                    DueDate = DateTime.Now,
-                    UpdatedAt = DateTime.Now
-                },
-                new TodoItem{
-                    Id = 2,
-                    Title = "Get a new pill box",
-                    IsCompleted = false,
-                    DueDate = DateTime.Parse("2022-05-22"),
-                    UpdatedAt = DateTime.Now
-                }
-            };
+        //    var todoList = new List<TodoItem>
+        //    {
+        //        new TodoItem{
+        //            Id = 1,
+        //            Title = "Call or go to YMCA and find out about May bill",
+        //            IsCompleted = false,
+        //            DueDate = DateTime.Now,
+        //            UpdatedAt = DateTime.Now
+        //        },
+        //        new TodoItem{
+        //            Id = 2,
+        //            Title = "Get a new pill box",
+        //            IsCompleted = false,
+        //            DueDate = DateTime.Parse("2022-05-22"),
+        //            UpdatedAt = DateTime.Now
+        //        }
+        //    };
 
-            TodoListView.ItemsSource = todoList;
-        }
+        //    TodoListView.ItemsSource = todoList;
+        //}
     }
 }
